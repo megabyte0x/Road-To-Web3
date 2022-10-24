d9934331baf9e339839ef2edb09e0001215c6de1
Megabyte-143 <yashygkf@gmail.com>
2022-10-24 18:20:18 +0530

    DONE with contracts

--- BuyMeACoffee-contracts/scripts/withdraw.js
+++ BuyMeACoffee-contracts/scripts/withdraw.js
@@ -1,3 +1,4 @@
+const { ethers } = require("hardhat");
 const hre = require("hardhat");
 const abi = require("../artifacts/contracts/BuyMeACoffee.sol/BuyMeACoffee.json");
 
@@ -11,16 +12,23 @@ async function main() {
     const contractAddress = "0x755172Ef01936279fD12006278718073Ee326a50";
     const contractABI = abi.abi;
 
-    const provider = new hre.ethers.providers.AlchemyProvider("polygon", process.env.POLYGON_API_KEY);
+    const provider = new hre.ethers.providers.AlchemyProvider("maticmum", process.env.POLYGON_API_KEY);
 
     const signer = new hre.ethers.Wallet(process.env.PRIVATE_KEY, provider);
+    const tipper1 = new hre.ethers.Wallet(process.env.TIPPER_PRIVATE_KEY, provider);
 
     const buyMeACoffee = new hre.ethers.Contract(contractAddress, contractABI, signer);
 
-    console.log("Current balance of owner: ", await getBalance(provider, signer.address), "ETH");
+    console.log("Current balance of owner: ", await getBalance(provider, signer.address), "MATIC");
+
+    console.log("Current balance of tipper: ", await getBalance(provider, tipper1.address), "MATIC");
+
+    console.log("Current balance of Contract: ", await getBalance(provider, buyMeACoffee.address), "MATIC");
+
+    const buyCofeeTx = await buyMeACoffee.connect(tipper1).buyCoffee("Patrick", "You are awesome", { value: ethers.utils.parseEther("0.1") });
+    await buyCofeeTx.wait();
 
     const contractBalance = await getBalance(provider, buyMeACoffee.address);
-    console.log("Current balance of Contract: ", contractBalance, "ETH");
 
     if (contractBalance !== "0.0") {
         console.log("Withdrawing Funds...");
