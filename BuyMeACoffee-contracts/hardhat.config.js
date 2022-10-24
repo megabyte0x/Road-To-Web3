f6db43c314a1419cfcdef2127cf03732ea1ccbd9
Megabyte-143 <yashygkf@gmail.com>
2022-10-24 16:47:33 +0530

    completed as Road to Web3

--- BuyMeACoffee-contracts/hardhat.config.js
+++ BuyMeACoffee-contracts/hardhat.config.js
@@ -1,6 +1,21 @@
-require("@nomicfoundation/hardhat-toolbox");
+require("@nomiclabs/hardhat-ethers");
+require("@nomiclabs/hardhat-waffle");
+require("dotenv").config()
 
-/** @type import('hardhat/config').HardhatUserConfig */
+
+const GOERLI_URL = process.env.GOERLI_URL;
+const PRIVATE_KEY = process.env.PRIVATE_KEY;
+
 module.exports = {
-  solidity: "0.8.17",
+  solidity: "0.8.4",
+  networks: {
+    goerli: {
+      url: GOERLI_URL,
+      accounts: [PRIVATE_KEY]
+    },
+    ploygon: {
+      url: POLYGON_URL,
+      accounts: [PRIVATE_KEY]
+    }
+  }
 };
